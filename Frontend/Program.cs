82d90d19ed39e5d8cf4b15df5e1905e4fa3fc0a1
FrejaMeyer <Frejme@gmail.com>
2025-05-29 17:52:54 +0200

    ny connectionstring

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient()
 {
-    BaseAddress = new Uri("http://localhost:3500/v1.0/invoke/")
+    BaseAddress = new Uri("http://localhost:30005")
 });
 
 builder.Services.AddScoped<BasketClientService>();
@@ -18,7 +18,7 @@ builder.Services.AddScoped<CartStateService>();
 builder.Services.AddScoped<CustomerSessionService>();
 builder.Services.AddScoped<PaymentService>();
 
-
+//http://localhost:3500/v1.0/invoke/
 
 builder.Services.AddBlazoredToast();
 
